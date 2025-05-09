41c019cc6e8f22d12eeb60d981a35cb2c74406dd
tonho <tonysousalopes555@gmail.com>
2025-05-09 10:01:10 -0300

    adicionando manutencoes

---
+++ manutencoes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from maquinas.models import Maquina
+
+class Manutencao(models.Model):
+    maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE, related_name='manutencoes', blank=False, null=False)
+    data = models.DateField(verbose_name="Data da Manutenção")
+    descricao = models.TextField(verbose_name="Descrição", blank=True, null=False)
+    pecas_usadas = models.CharField(verbose_name="Peças Utilizadas", max_length=255, blank=True)
+    custo = models.DecimalField(verbose_name="Custo Total (R$)", max_digits=10, decimal_places=2, default=0)
+
+    class Meta:
+        ordering = ['-data']
+
+    def __str__(self):
+        return f'{self.maquina.nome} - {self.data}'
---
+++ manutencoes/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
+from django.utils.dateparse import parse_date
+from .models import Manutencao
+
+
+class ManutencaoListView(ListView):
+    model = Manutencao
+    context_object_name = 'manutencoes'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get('search')
+        data_inicio = self.request.GET.get('data_inicio')
+        data_fim = self.request.GET.get('data_fim')
+
+        if search:
+            queryset = queryset.filter(descricao__icontains=search)
+
+        if data_inicio:
+            queryset = queryset.filter(data__gte=parse_date(data_inicio))
+        if data_fim:
+            queryset = queryset.filter(data__lte=parse_date(data_fim))
+
+        return queryset
+    
+class ManutencaoCreateView(CreateView):
+    model = Manutencao
+    fields = ['maquina', 'data', 'descricao', 'pecas_usadas', 'custo']
+    template_name = 'manutencoes/manutencao_form.html'
+    success_url = reverse_lazy('lista_manutencoes')
+
+
+class ManutencaoUpdateView(UpdateView):
+    model = Manutencao
+    fields = ['maquina', 'data', 'descricao', 'pecas_usadas', 'custo']
+    success_url = reverse_lazy('lista_manutencoes')
+
+class ManutencaoDeleteView(DeleteView):
+    model = Manutencao
+    success_url = reverse_lazy('lista_manutencoes')
